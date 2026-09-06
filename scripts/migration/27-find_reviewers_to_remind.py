d6a03f1dccff44a3a5e8681055fcf42779111439
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-09-06 22:13:15 +0200

    better reviewer filtering

--- scripts/migration/27-find_reviewers_to_remind.py
+++ scripts/migration/27-find_reviewers_to_remind.py
@@ -8,7 +8,7 @@ async def main():
     users = await get_users()
     submissions = await get_submissions()
 
-    naive_two_weeks_ago = datetime.now() - timedelta(days=14)
+    two_weeks_ago = datetime.now() - timedelta(days=14)
 
     for user in users:
         if "reviewer" not in user["roles"]:
@@ -20,19 +20,36 @@ async def main():
         username = user["username"]
         
         accepted_subs = [sub for sub in submissions if sub["user_id"] == user["id"] and sub["status"] == "accept"]
-        if len(accepted_subs) < 10:
+        # FILTER: accepted at least 5 accepted submissions
+        if len(accepted_subs) < 5:
             continue
 
+        coverage = [lang.lower() for lang in user["review_langs"]]
+            
+        potential_subs = 0
+        for sub in submissions:
+            if sub["status"] == "pending" and sub["user_id"] != user["id"]:
+                x = sub["source_lang"].lower()
+                y = sub["target_lang"].lower()
+                x_match = any(lang in x or x in lang for lang in coverage)
+                y_match = any(lang in y or y in lang for lang in coverage)
+                if x_match or y_match:
+                    potential_subs += 1
+
+        # FILTER: has enough potential submissions to review
+        if potential_subs < 10:
+            continue
         
         latest_email_date_str = await get_latest_sent_email_date(user["email"], REVIEW_REMINDER_SUBJECT)
         if latest_email_date_str:
             latest_email_date = datetime.fromisoformat(latest_email_date_str).replace(tzinfo=None)
-            if latest_email_date >= naive_two_weeks_ago:
+            if latest_email_date >= two_weeks_ago:
                 continue
 
         reviewed_subs = [sub for sub in submissions if sub["reviewed_by"] == username]
 
-        if len(reviewed_subs) < 2:
+        # FILTER: has reviewed in the past
+        if len(reviewed_subs) < 1:
             continue
         
         last_review_date = datetime.min
@@ -43,8 +60,8 @@ async def main():
                     if dt > last_review_date:
                         last_review_date = dt
 
-        if last_review_date != datetime.min and last_review_date < naive_two_weeks_ago:
-            print(f"{user['name']} ({username})")
+        if last_review_date != datetime.min and last_review_date < two_weeks_ago:
+            print(f"{user['name']:<30} | Accepted: {len(accepted_subs):<3} | Reviewed: {len(reviewed_subs):<3} | Potential: {potential_subs:<3}")
 
 if __name__ == "__main__":
     asyncio.run(main())
